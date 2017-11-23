4cafdd8aa97a9f143a68eb0f66dbb825f47a804d
gabrielabud <gabriela.budeanu@gmail.com>
2017-11-23 17:07:30 +0000

    Adding selecting temperature

--- jq.js
+++ jq.js
@@ -36,8 +36,12 @@ $(document).ready(function() {
   function updateTemperature() {
   $('#temperat').text(thermostat.temperature);
   $('#temperat').attr('class', thermostat.energyUsage());
-}
+  }
 
-
-
+  $('#sel-city').change(function() {
+  var city = $('#sel-city').val();
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=ff49585b94915de068db1f6cdea6f7f3&units=metric', function(data) {
+    $('#temperature-outside').text(data.main.temp)
+  })
+  })
 });
